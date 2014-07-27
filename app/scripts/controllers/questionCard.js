3ebde3e3adcbf5268be5fa738548ea9b4660d3c3
Alexey Tarasenko <altarko@gmail.com>
2014-07-27 18:34:11 +0400

    Создана форма входа с проверкой полей Вопросы подгружаются с базы sweb посредствам JSOP Добавлен индикатор загрузки ngProgress

--- app/scripts/controllers/questionCard.js
+++ app/scripts/controllers/questionCard.js
@@ -7,12 +7,36 @@
  * # QuestionCard
  * Controller of the sportTestApp
  */
-angular.module('sportTestApp')
-    .controller('QuestionCard', function ($scope, Questiondata) {
+angular.module('sportTestApp', ['ngProgress'])
+    .controller('QuestionCard', function ($scope, $http, ngProgress) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-        this.questions = Questiondata.questions;
-    });
+
+        //курс студента
+        $scope.curriculum = 3;
+
+        //запускаем прогрессбар
+        ngProgress.color('green');
+        ngProgress.start();
+
+        var config = {
+            params: {
+                curriculum: $scope.curriculum,
+                limit: 5,
+                callback: 'JSON_CALLBACK'
+            }
+        };
+
+        //запрашиваем данные с сервера
+        var url = 'http://brothersdesign.ru/sporttest/requests/angular.php?';
+        $http.jsonp(url, config).success(function(data){
+            //останавливаем прогрессбар
+            ngProgress.complete();
+            $scope.questions = data;
+        });
+
+        //ответ от студента
+        $scope.answerFromStudent = '';    });
